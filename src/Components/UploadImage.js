7d7bd492678dfbb01a353dda643e0efd9a7d3e6c
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2025-02-10 14:51:50 +0530

    cloudinary preset changed

--- src/Components/UploadImage.js
+++ src/Components/UploadImage.js
@@ -6,7 +6,7 @@ const url = `https://api.cloudinary.com/v1_1/dssnjojxu/image/upload`
 const uploadImage = async (image) => {
     const formData = new FormData()
     formData.append("file", image)
-    formData.append("upload_preset","blogs")
+    formData.append("upload_preset","showmanblog")
     const response = await axios.post(url, formData)
     return response.data
 }
