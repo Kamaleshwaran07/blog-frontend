2e7a6a90657ba3bfcafc64d7a353038aea579999
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2025-02-11 20:52:35 +0530

    description loading changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,18 +5,21 @@ import axios from "axios";
 import { myContext } from "../Context/Authcontext.jsx";
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [desc, setDesc] = useState("")
   const { baseurl } = useContext(myContext);
   /*Extracting the category */
   const cat = useLocation().search;
-  const getDocument = (html) => {
+  const getDocument = () => {
+    const html = posts[2]
     const htmlDoc = new DOMParser().parseFromString(html, "text/html");
-    return htmlDoc.body.innerHTML;
+    setDesc(htmlDoc.body.innerHTML);
   };
   /* Fetching the posts data*/
   const fetchdata = async () => {
     const res = await axios.get(`${baseurl}/api/posts/${cat}`);
 
     setPosts(res.data);
+    
     // console.log(res.data);
   };
   const navigate = useNavigate();
@@ -44,7 +47,7 @@ const Home = () => {
               <Link to={`/post/${post.id}`}>
                 <h1 className="text-5xl">{post.title}</h1>
               </Link>
-              <p className="text-xl line-clamp-2">{getDocument(post.description)}</p>
+              <p className="text-xl line-clamp-2" dangerouslySetInnerHTML={{__html:desc}}></p>
               <button
                 className="w-max px-2 py-2 border border-teal-500 text-teal-500 hover:border-white hover:text-black hover:bg-lightgreen"
                 onClick={() => navigate(`/post/${post.id}`)}
